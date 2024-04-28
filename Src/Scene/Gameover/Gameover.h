a1801efc809dd3d66370418ed49b0ef0af8b4535
tyokusenbantyo <144639918+tyokusenbantyo@users.noreply.github.com>
2024-04-29 01:32:02 +0900

    ゲームオーバーシーン完成

--- Src/Scene/Gameover/Gameover.h
+++ Src/Scene/Gameover/Gameover.h
@@ -4,9 +4,26 @@ class SceneGameOver {
 private:
 	//背景画像
 	int BackGround_img;
+	int score_text_img;
+
+	int kari_highscore_img;
+
+	int Number_handle[10];
+	float numberX, numberY;
+
+	int kari_score;
 public:
 	SceneGameOver();
 	~SceneGameOver();
+	int Digit_1 = -1;
+	int Digit_2 = -1;
+	int Digit_3 = -1;
+	int Digit_4 = -1;
+	int Digit_5 = -1;
+	int Digit_6 = -1;
+	int Digit_7 = -1;
+	int Digit_8 = -1;
+	int Digit_9 = -1;
 
 	void InitGameOver();		// ゲームオーバー初期化
 
@@ -15,4 +32,8 @@ public:
 	void DrawGameOver();		// ゲームオーバー描画処理
 
 	void FinGameOver();		    // ゲームオーバー終了処理
+
+	void SetPos(int num1, int num2);
+
+	void DrawFont(int number);
 };
--- Src/Scene/Gameover/Gamever.cpp
+++ Src/Scene/Gameover/Gamever.cpp
@@ -2,11 +2,16 @@
 #include "Gameover.h"
 #include "../Scene.h"
 #include "../../Input/Input.h"
+#include "string"
 
 #define  RESULT_PATH "data/Clearimage/result.png"
+#define  RESULT_TEXT_PATH "data/Clearimage/Number.png"
+#define  RESULT_HIGH_SCORE_PATH "data/Clearimage/kari_highscore.png"
 
+using namespace std;
 SceneGameOver::SceneGameOver() {
-	BackGround_img = 0;
+    kari_score = 0;
+    kari_highscore_img = 0;
 }
 
 SceneGameOver::~SceneGameOver() {}
@@ -15,10 +20,16 @@ SceneGameOver::~SceneGameOver() {}
 void SceneGameOver::InitGameOver() {
 	//画像読み込み
 	BackGround_img = LoadGraph(RESULT_PATH);
+	score_text_img = LoadGraph(RESULT_TEXT_PATH);
+    kari_highscore_img = LoadGraph(RESULT_HIGH_SCORE_PATH);
+	
+    kari_score = 12;
 
+    
+
+    LoadDivGraph("data/Clearimage/Number.png", 10, 10, 1, 30, 60, Number_handle);
 
 	g_CurrentSceneID = SCENE_ID_LOOP_GAMEOVER;
-	
 }
 
 // ゲームオーバー通常処理
@@ -26,15 +37,58 @@ void SceneGameOver::StepGameOver() {
 	if (IsKeyPush(KEY_INPUT_RETURN)) {
 		g_CurrentSceneID = SCENE_ID_FIN_GAMEOVER;
 	}
-
+    SetPos(450, 370);
 }
 
 // ゲームオーバー描画処理
 void SceneGameOver::DrawGameOver() {
 	DrawGraph(0, 0, BackGround_img, true);
+    DrawGraph(210, 495, kari_highscore_img, true);
+   
+    DrawFont(kari_score);
 }
 
 //ゲームオーバー終了処理
 void SceneGameOver::FinGameOver() {
+    
 	g_CurrentSceneID = SCENE_ID_INIT_TITLE;
+}
+
+
+void SceneGameOver::SetPos(int num1, int num2)
+{
+    numberX = num1;
+    numberY = num2;
+}
+
+void SceneGameOver::DrawFont(int number)
+{
+    Digit_1 = number % 10;
+    Digit_2 = number / 10 % 10;
+    Digit_3 = number / 100 %10;
+    Digit_4 = number / 1000%10;
+    Digit_5 = number / 10000%10;
+    Digit_6 = number / 100000%10;
+    Digit_7 = number / 1000000%10;
+    Digit_8 = number / 10000000%10;
+    Digit_9 = number / 100000000;
+
+    DrawGraph(numberX, numberY, Number_handle[Digit_1], true);
+
+    DrawGraph(numberX-30, numberY, Number_handle[Digit_2], true);
+
+    DrawGraph(numberX - 60, numberY, Number_handle[Digit_3], true);
+    
+    DrawGraph(numberX - 90, numberY, Number_handle[Digit_4], true);
+    
+    DrawGraph(numberX - 120, numberY, Number_handle[Digit_5], true);
+    
+    DrawGraph(numberX - 150, numberY, Number_handle[Digit_6], true);
+    
+    DrawGraph(numberX - 180, numberY, Number_handle[Digit_7], true);
+   
+    DrawGraph(numberX - 210, numberY, Number_handle[Digit_8], true);
+   
+    DrawGraph(numberX - 240, numberY, Number_handle[Digit_9], true);
+    
 }
